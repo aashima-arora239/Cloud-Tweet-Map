89ef9c151b4bab919e58d325b526c3d733ccfa4a
System Administrator <root@dyn-160-39-195-251.dyn.columbia.edu>
2016-10-24 21:09:32 -0400

    Final Checkin

---
+++ mysite/credentials.py
@@ -0,0 +1,10 @@
+consumer_key="L2edGlDjo2FUxRSUL1S2mh8gY"
+consumer_secret="rTdVnUGJ6FP0Czr67vj9ELwr3m1kml2Bg26orTRodu60URBj5O"
+access_token="789927305804218368-E8Uj5Ct5H5BMYrEGEFTHu6uNZFFxdls"
+access_token_secret="AZtW0krPGCoQs4OwGsKMRSmpGTeej3iBev3vvqL6B9MdC"
+
+es_host = ['https://search-jask-tweetmap-hhk4izgywmbpwob2zah4fcdiry.us-west-2.es.amazonaws.com']
+aws_region = "us-west-2"
+sqs_name ="twitter"
+aws_id ="633550000938 "
+aws_key= "AKIAJGWM4EIIRJQW5MOQ"
